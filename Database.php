a739599f34c2408738762b8201ce9be3fe3d83ab
Your Name <your-email@example.com>
2025-01-15 23:09:56 +1000

    s

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@ class Database {
     private $host = "localhost";
     private $db_name = "u787474055_wantokpay2";  // Your existing database name
     private $username = "u787474055_panther707"; // Your existing username
-    private $password = "Blackpanther707@";      // Your existing password
+    private $password = "Blackpanther707@707";      // Your existing password
     public $conn;
     
     public function getConnection() {
