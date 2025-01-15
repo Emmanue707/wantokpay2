4928a5b983a15f9d938417391964c8ae85c91684
Your Name <your-email@example.com>
2025-01-15 23:28:00 +1000

    s

--- index.php
+++ index.php
@@ -106,5 +106,7 @@ session_start();
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
